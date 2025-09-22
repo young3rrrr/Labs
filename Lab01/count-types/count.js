d23944c490ca6c1ef425184883c4bfae54420d31
youngerrrr <uuuunger@gmail.com>
2025-09-22 10:43:42 +0300

    some changes

--- Lab01/count-types/count.js
+++ Lab01/count-types/count.js
@@ -4,9 +4,13 @@ function countTypes(arr) {
         if (type in Types) {
             Types[type]++;
         }
+        else{
+             Types[type] = 1; 
+        }
     }
 }
-const Types = { number: 0, string: 0, boolean: 0 };
-const array = [1, "hello", true, 42, "world", false, 3.14, "JavaScript", true, 100, "code", false, 7, "test", true, 0, "end", 99, "types", false, 2.71];
+const Types = {};
+const array = [1, "hello", true, 42, "world", false, 3.14, "JavaScript", true, 100, "code", false, 7, "test", true, 0, "end", 99, "types", false, 2.71, BigInt(9007199254740991),
+     Symbol("sym"), null, undefined, function() {}, {}, []];
 countTypes(array);
 console.dir(Types);
--- Lab02/Collections/Collections.js
+++ Lab02/Collections/Collections.js
@@ -16,6 +16,20 @@ function findPhoneByName(name) {
     return 'Контакт не знайдено';
 }
 
+function findNameByPhone(phone) {
+    const contact = phoneBook.find(c => c.phone === phone);
+    if (contact) {
+        console.log(contact.name);
+        return contact.name;
+    }
+    console.log('Контакт не знайдено');
+    return 'Контакт не знайдено';
+}
+
 findPhoneByName('Jane Smith'); // '+1987654321'
 findPhoneByName('Alice Johnson'); // '+1122334455'
 findPhoneByName('Bob Brown'); // 'Контакт не знайдено'
+
+findNameByPhone('+1234567890'); // 'John Doe'
+findNameByPhone('+1122334455'); // 'Alice Johnson'
+findNameByPhone('+0000000000'); // 'Контакт не знайдено'    
